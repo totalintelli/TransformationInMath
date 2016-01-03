fb75b315d1bb2f6846784086c24f687dec50a143
blueintelli <blueintelli@gmail.com>
2016-01-03 21:49:52 +0900

    Make Class about real number to common log number and common log number to real number. Make test  class about this class.

---
+++ Transformation/LogTransformation.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transformation
+{
+    public class LogTransformation
+    {
+        public enum LogType
+        {
+            CommonLog, // 밑이 10인 로그, 상용 로그
+            BinaryLog, // 밑이 2인 로그
+            NatualLog // 밑이 e인 로그, 자연 로그
+        }
+
+
+
+
+        /// <summary>
+        /// 숫자를 십진 로그 값으로 바꾸는 함수
+        /// </summary>
+        /// <param name="input">입력한 숫자</param>
+        /// <param name="min">입력한 숫자가 속한 범위의 최소값</param>
+        /// <param name="max">입력한 숫자가 속한 범위의 최대값</param>
+        /// <param name="logType">로그의 종류</param>
+        /// <returns></returns>
+        public float NumToLog(float input, float min, float max, LogType logType = LogType.CommonLog)
+        {
+            float result = 0;
+
+            if (input >= min && input <= max)
+            {
+                result = Convert.ToSingle(Math.Log10(input));
+            }
+
+            if(float.IsInfinity(result) || input < min || input > max || input == 0)
+            {
+                result = float.NaN;
+            }
+
+            if(input < 0)
+            {
+                result = Convert.ToSingle(Math.Log10(Math.Abs(input)) * (-1));
+            }
+
+            return result;
+        }
+
+
+
+
+        /// <summary>
+        /// 십진 로그 값을 숫자로 바꾸는 함수
+        /// </summary>
+        /// <param name="input">입력한 로그 값</param>
+        /// <param name="min">입력한 로그 값이 속한 범위의 최소값</param>
+        /// <param name="max">입력한 로그 값이 속한 범위의 최대값</param>
+        /// <param name="isPreviousNumberNegative">입력한 로그 값이 된 숫자가 음수인지의 여부</param>
+        /// <param name="logType">입력한 로그 값의 로그 종류</param>
+        /// <returns></returns>
+        public float LogToNum(float input, float min, float max, bool isPreviousNumberNegative, LogType logType = LogType.CommonLog)
+        {
+            float result = 0;
+
+            if(input > 0)
+            {
+                result = Convert.ToSingle(Math.Pow(10, Convert.ToDouble(input)));
+            }
+            else
+            {
+                if(isPreviousNumberNegative == true)
+                {
+                    result = Convert.ToSingle(Math.Pow(10, Convert.ToDouble(Math.Abs(input))) * (-1));
+                }
+                else
+                {
+                    result = Convert.ToSingle(Math.Pow(10, Convert.ToDouble(input)));
+                }
+                
+            }
+
+            return result;
+        }
+    }
+}
---
+++ TransformationTest/TransformationTest.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Transformation;
+namespace TransformationTest
+{
+    [TestClass]
+    public class TransformationTest
+    {
+        [TestMethod]
+        public void WhenPositiveNumber_ShouldReturnValue()
+        {
+            LogTransformation logTrans = new LogTransformation();
+            float expectedResult = 1.0f;
+            float input = 10;
+            float min = 1;
+            float max = 11;
+            
+            float actualResult = logTrans.NumToLog(input, min, max);
+            Assert.AreEqual(expectedResult, actualResult);
+
+            expectedResult = 3.30103f;
+            input = 2000;
+            min = 0;
+            max = 10000;
+            actualResult = logTrans.NumToLog(input, min, max);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+
+        [TestMethod]
+        public void WhenMiniPositiveNumber_ShouldReturnMinusNumber()
+        {
+            LogTransformation logTrans = new LogTransformation();
+            float expectedResult = -4.853872f;
+            float input = 0.000014f;
+            float min = 0;
+            float max = 1;
+            float actualResult = logTrans.NumToLog(input, min, max);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+
+        [TestMethod]
+        public void WhenMinusNumber_ShouldReturnMinusNumber()
+        {
+            LogTransformation logTrans = new LogTransformation();
+            float expectedResult = -1;
+            float input = -10;
+            float min = -9;
+            float max = -11;
+            float actualResult = logTrans.NumToLog(input, min, max);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+        [TestMethod]
+        public void WhenZero_ShouldReturnNaN()
+        {
+            LogTransformation logTrans = new LogTransformation();
+            float expectedResult = float.NaN;
+            float input = 0;
+            float min = -1;
+            float max = 1;
+            float actualResult = logTrans.NumToLog(input, min, max);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+
+        [TestMethod]
+        public void WhenOutOfRangeNumber_ShouldReturnNaN()
+        {
+            LogTransformation logTrans = new LogTransformation();
+            float expectedResult = float.NaN;
+            float input = 3;
+            float min = 4;
+            float max = 5;
+            float actualResult = logTrans.NumToLog(input, min, max);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+
+        [TestMethod]
+        public void WhenPositiveLogVaule_ShouldReturnValue()
+        {
+            LogTransformation logTrans = new LogTransformation();
+            float expectedResult = 10;
+            float input = 1;
+            float min = 0;
+            float max = 10;
+            bool isPreviousNumberNegative = false;
+            float actualResult = logTrans.LogToNum(input, min, max, isPreviousNumberNegative);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+
+        [TestMethod]
+        public void WhenMinusLogValue_ShouldReturnMinusValue()
+        {
+            LogTransformation logTrans = new LogTransformation();
+            float expectedResult = -10;
+            float input = -1;
+            float min = -2;
+            float max = 0;
+            bool isPreviousNumberNegative = true;
+            float actualResult = logTrans.LogToNum(input, min, max, isPreviousNumberNegative);
+            Assert.AreEqual(expectedResult, actualResult);
+
+            expectedResult = 0.000014f;
+            input = -4.853872f;
+            min = -3;
+            max = -5;
+            isPreviousNumberNegative = false;
+            actualResult = logTrans.LogToNum(input, min, max, isPreviousNumberNegative);
+            Assert.AreEqual(expectedResult, actualResult, delta: expectedResult / 1000000);
+        }
+    }
+}
