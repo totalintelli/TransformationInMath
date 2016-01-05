cecf670a991c8501d9f1a09d84133e9dfec2e925
totalintelli <totalintelli@hotmail.com>
2016-01-06 02:54:15 +0900

    테스트할 숫자 값 수정

--- TransformationTest/TransformationTest.cs
+++ TransformationTest/TransformationTest.cs
@@ -12,16 +12,16 @@ namespace TransformationTest
             LogTransformation logTrans = new LogTransformation();
             float expectedResult = 1.0f;
             float input = 10;
-            float min = 1;
-            float max = 11;
+            float min = 0;
+            float max = 43.78f;
             
             float actualResult = logTrans.NumToLog(input, min, max);
             Assert.AreEqual(expectedResult, actualResult);
 
-            expectedResult = 3.30103f;
-            input = 2000;
+            expectedResult = 3;
+            input = 1000;
             min = 0;
-            max = 10000;
+            max = 1779;
             actualResult = logTrans.NumToLog(input, min, max);
             Assert.AreEqual(expectedResult, actualResult);
         }
@@ -48,8 +48,8 @@ namespace TransformationTest
             LogTransformation logTrans = new LogTransformation();
             float expectedResult = -1;
             float input = -10;
-            float min = -9;
-            float max = -11;
+            float min = -1235;
+            float max = -5;
             float actualResult = logTrans.NumToLog(input, min, max);
             Assert.AreEqual(expectedResult, actualResult);
         }
@@ -90,9 +90,15 @@ namespace TransformationTest
             float expectedResult = 10;
             float input = 1;
             float min = 0;
-            float max = 10;
-            bool isPreviousNumberNegative = false;
-            float actualResult = logTrans.LogToNum(input, min, max, isPreviousNumberNegative);
+            float max = 1779;
+            float actualResult = logTrans.LogToNum(input, min, max);
+            Assert.AreEqual(expectedResult, actualResult);
+
+            expectedResult = 0.1f;
+            input = -1;
+            min = 0.1170848f;
+            max = 1.09512f;
+            actualResult = logTrans.LogToNum(input, min, max);
             Assert.AreEqual(expectedResult, actualResult);
         }
 
@@ -102,21 +108,27 @@ namespace TransformationTest
         public void WhenNegativeLogValue_ShouldReturnMinusValue()
         {
             LogTransformation logTrans = new LogTransformation();
-            float expectedResult = -10;
-            float input = -1;
-            float min = -2;
-            float max = 0;
-            bool isPreviousNumberNegative = true;
-            float actualResult = logTrans.LogToNum(input, min, max, isPreviousNumberNegative);
+            float expectedResult = -1000;
+            float input = -3;
+            float min = -1235;
+            float max = -5;
+            float actualResult = logTrans.LogToNum(input, min, max);
             Assert.AreEqual(expectedResult, actualResult);
 
-            expectedResult = 0.000014f;
-            input = -4.853872f;
-            min = -3;
-            max = -5;
-            isPreviousNumberNegative = false;
-            actualResult = logTrans.LogToNum(input, min, max, isPreviousNumberNegative);
-            Assert.AreEqual(expectedResult, actualResult, delta: expectedResult / 1000000);
+            expectedResult = -10;
+            input = -1;
+            min = -100;
+            max = -2;
+            actualResult = logTrans.LogToNum(input, min, max);
+            Assert.AreEqual(expectedResult, actualResult);
+
+            expectedResult = -100;
+            input = -2;
+            min = -1000;
+            max = 2990;
+            actualResult = logTrans.LogToNum(input, min, max);
+            Assert.AreEqual(expectedResult, actualResult);
+
         }
     }
 }
