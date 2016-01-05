53b45bec343d02f91e536650ad7053495f05ac4f
totalintelli <totalintelli@hotmail.com>
2016-01-06 02:53:57 +0900

    함수 로직 수정

--- Transformation/LogTransformation.cs
+++ Transformation/LogTransformation.cs
@@ -19,7 +19,7 @@ namespace Transformation
 
 
         /// <summary>
-        /// 숫자를 십진 로그 값으로 바꾸는 함수
+        /// 숫자를 상용로그 값으로 바꾸는 함수
         /// </summary>
         /// <param name="input">입력한 숫자</param>
         /// <param name="min">입력한 숫자가 속한 범위의 최소값</param>
@@ -28,22 +28,23 @@ namespace Transformation
         /// <returns></returns>
         public float NumToLog(float input, float min, float max, LogType logType = LogType.CommonLog)
         {
-            float result = 0;
+            float result = float.NaN;
 
             if (input >= min && input <= max)
             {
                 result = Convert.ToSingle(Math.Log10(input));
             }
 
-            if(float.IsInfinity(result) || input < min || input > max || input == 0)
-            {
-                result = float.NaN;
-            }
-
-            if(input < 0)
+            if (input < 0)
             {
                 result = Convert.ToSingle(Math.Log10(Math.Abs(input)) * (-1));
             }
+            
+            // 무한대 값 처리
+            if(float.IsInfinity(result))
+            {
+                result = float.NaN;
+            }           
 
             return result;
         }
@@ -52,7 +53,7 @@ namespace Transformation
 
 
         /// <summary>
-        /// 십진 로그 값을 숫자로 바꾸는 함수
+        /// 상용로그 값을 숫자로 바꾸는 함수
         /// </summary>
         /// <param name="input">입력한 로그 값</param>
         /// <param name="min">입력한 로그 값이 속한 범위의 최소값</param>
@@ -60,25 +61,24 @@ namespace Transformation
         /// <param name="isPreviousNumberNegative">입력한 로그 값으로 바꾼 숫자가 음수인지의 여부</param>
         /// <param name="logType">입력한 로그 값의 로그 종류</param>
         /// <returns></returns>
-        public float LogToNum(float input, float min, float max, bool isPreviousNumberNegative, LogType logType = LogType.CommonLog)
+        public float LogToNum(float input, float min, float max, LogType logType = LogType.CommonLog)
         {
-            float result = 0;
+            float result = float.NaN;
 
             if(input > 0)
             {
-                result = Convert.ToSingle(Math.Pow(10, Convert.ToDouble(input)));
+                result = Convert.ToSingle(Math.Pow(10, input));
             }
             else
             {
-                if(isPreviousNumberNegative == true)
+                if(input > max || input > min)
                 {
-                    result = Convert.ToSingle(Math.Pow(10, Convert.ToDouble(Math.Abs(input))) * (-1));
+                    result = Convert.ToSingle(Math.Pow(10, Math.Abs(input))) * (-1);
                 }
-                else
+                else if(input < min || input < max)
                 {
-                    result = Convert.ToSingle(Math.Pow(10, Convert.ToDouble(input)));
+                    result = Convert.ToSingle(Math.Pow(10, input));
                 }
-                
             }
 
             return result;
